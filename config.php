1c88853f94300f63e3c08a9b9156b2e21bc91514
Agradeep Khanra <agradeep92@gmail.com>
2016-08-15 21:22:58 -0400

    Final commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "123456");
+define("DB_NAME", "shareboard");
+
+// Define URL
+define("ROOT_PATH", "/php.dev/");
+define("ROOT_URL", "http://localhost/php.dev/");
